4c5be213b8af24c7b02287b34b0d6fcf6e09722d
Álvaro <alvarohdezex@gmail.com>
2024-11-14 13:57:50 +0100

    added haskell support you in case you know

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -29,6 +29,7 @@ return {
                 typescriptreact = { "prettier" },
                 javascriptreact = { "prettier" },
                 rust = { "rustfmt" },
+                haskell = { "ormolu" },
             },
             format_on_save = {
                 timeout_ms = 500,
