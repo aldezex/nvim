7c924fa3a82f7c5bb396fa0977ec218c5ad8b5ab
Álvaro <alvarohdezex@gmail.com>
2025-09-26 12:54:24 +0200

    added tabs and tree

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("config.settings")
 require("config.keymaps")
 
 require("config.lazy")
+
+require("config.tree")
---
+++ lua/plugins/tabs.lua
@@ -0,0 +1,8 @@
+return {
+    "romgrk/barbar.nvim",
+    version = "^1.0.0",
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+}
